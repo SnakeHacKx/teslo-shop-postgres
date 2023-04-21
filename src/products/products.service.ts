6424a56b06dcd76ba5a2c669a9142d969532168d
snakehackx <omaralexisxdx2@gmail.com>
2023-04-21 15:43:43 -0500

    Commit antes de pasar a linux

---
+++ src/products/products.service.ts
@@ -0,0 +1,84 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { validate as isUUID } from 'uuid';
+
+@Injectable()
+export class ProductsService {
+  private readonly logger = new Logger('ProductService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto; // Valores por defecto de los queryParams
+    return this.productRepository.find({
+      take: limit,
+      skip: offset
+    })
+      // .skip(offset)
+      // .sort({ no: 1 }) // Ordena la columna no de forma ascendente
+      // .select('-__v');
+  }
+
+  async findOne(term: string) {
+
+    let product: Product;
+    // const product = await this.productRepository.findOneBy({ term })
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({id: term});
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+
+    if (!product) {
+      throw new NotFoundException(`Product with ${term} not found`);
+    }
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+
+    await this.productRepository.remove(product);
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
+  }
+}
