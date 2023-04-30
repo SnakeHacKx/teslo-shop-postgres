67f794f98db138e084cac6f638b5937d7bed90bd
SnakeHacKx <omaralexisxdx2@gmail.com>
2023-04-29 21:18:35 -0500

    Casi terminada la autenticacion

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+  SetMetadata,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from './entities/user.entity';
+import { Auth, GetUser, RawHeaders, RoleProtected } from './decorators';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -19,10 +34,41 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+  ) {
+    console.log({ request });
+    console.log({ user });
+
     return {
       ok: true,
-      message: 'Private route'
-    }
+      message: 'Private route',
+      user,
+      userEmail,
+      rawHeaders,
+    };
+  }
+
+  @Get('private2')
+  // @SetMetadata('roles', ['admin', 'superuser'])
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.superUser)
+  privateRoute3(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
   }
 }
--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SeedService } from './seed.service';
 
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
+
+import { SeedService } from './seed.service';
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
+  @Auth(ValidRoles.admin)
   async executeSeed() {
     return this.seedService.runSeed();
   }
